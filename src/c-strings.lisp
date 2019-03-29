a1a3c34086998784469742503ff9e3b5930bbe68
Teemu Likonen <tlikonen@iki.fi>
2019-03-29 15:13:44 +0200

    Update src/c-strings.lisp

--- src/c-strings.lisp
+++ src/c-strings.lisp
@@ -20,12 +20,12 @@ character codes."
     (handler-case
         (with-input-from-string (in string)
 
-          (flet ((read-chars (stream predicate max)
+          (flet ((read-chars (predicate max)
                    (with-output-to-string (chars)
                      (loop :repeat max
-                           :for c := (peek-char nil stream)
+                           :for c := (peek-char nil in)
                            :do (if (funcall predicate c)
-                                   (princ (read-char stream) chars)
+                                   (princ (read-char in) chars)
                                    (loop-finish)))))
 
                  (hex-char-p (char)
@@ -43,37 +43,37 @@ character codes."
 
                 (let ((sub-char (read-char in)))
                   (case sub-char
-                    (#\a (princ (code-char #x07) out))
-                    (#\b (princ #\backspace out))
-                    (#\e (princ (code-char #x1b) out))
-                    (#\f (princ (code-char #x0c) out))
-                    (#\n (princ #\newline out))
-                    (#\r (princ #\return out))
-                    (#\t (princ #\tab out))
-                    (#\v (princ (code-char #x0b) out))
+                    (#\a (princ #\Bel out))
+                    (#\b (princ #\Backspace out))
+                    (#\e (princ #\Esc out))
+                    (#\f (princ #\Page out))
+                    (#\n (princ #\Newline out))
+                    (#\r (princ #\Return out))
+                    (#\t (princ #\Tab out))
+                    (#\v (princ #\Vt out))
                     (#\\ (princ #\\ out))
                     (#\' (princ #\' out))
                     (#\" (princ #\" out))
                     (#\? (princ #\? out))
 
                     (#\u
-                     (let ((cp (read-chars in #'hex-char-p 4)))
+                     (let ((cp (read-chars #'hex-char-p 4)))
                        (when (= 4 (length cp))
                          (princ (code-char (parse-int-base 16 cp)) out))))
 
                     (#\U
-                     (let ((cp (read-chars in #'hex-char-p 8)))
+                     (let ((cp (read-chars #'hex-char-p 8)))
                        (when (= 8 (length cp))
                          (princ (code-char (parse-int-base 16 cp)) out))))
 
                     (#\x
-                     (let ((cp (read-chars in #'hex-char-p 2)))
+                     (let ((cp (read-chars #'hex-char-p 2)))
                        (when (plusp (length cp))
                          (princ (code-char (parse-int-base 16 cp)) out))))
 
                     (t
                      (unread-char sub-char in)
-                     (let ((cp (read-chars in #'octal-char-p 3)))
+                     (let ((cp (read-chars #'octal-char-p 3)))
                        (when (plusp (length cp))
                          (princ (code-char (parse-int-base 8 cp)) out))))))
 
