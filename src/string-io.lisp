9c09d975983dfd83b1bd4dc8a4c8a068a4e11f92
Teemu Likonen <tlikonen@iki.fi>
2019-06-17 15:56:04 +0300

    Update string-io.lisp

--- src/string-io.lisp
+++ src/string-io.lisp
@@ -12,7 +12,7 @@
            #:escape-characters
            #:quote-string
            #:sql-string
-           #:sql-like-escape
+           #:sql-escape-like
            #:unescape-c-string))
 
 (in-package #:string-io)
@@ -21,9 +21,14 @@
   ((string :reader parsed-string :initarg :string))
   (:report "The end of file was reached before closing quote character."))
 
+(defun make-adjustable-string (length)
+  (make-array length :element-type 'character
+                     :adjustable t :fill-pointer 0))
+
 (defun parse-quoted-word (string-or-stream
                           &key output-stream
-                            (quote-char #\") (escape-char #\\)
+                            (quote-start-char #\") (quote-end-char #\")
+                            (escape-char #\\)
                             (separator-chars '(#\space #\tab))
                             (unescape t))
 
@@ -31,34 +36,43 @@
 SEPARATOR-CHARS. If nothing could be parsed return NIL. If OUTPUT-STREAM
 is a stream write also output to that stream.
 
-If QUOTE-CHAR is a character then characters surrounded by QUOTE-CHARs
-are part of a word. Also, if ESCAPE-CHAR is non-NIL then characters that
-come right after ESCAPE-CHAR are part of a word.
+If QUOTE-START-CHAR and QUOTE-END-CHAR are characters then all
+characters surrounded by them are part of a word. Also, if ESCAPE-CHAR
+is non-NIL then characters that come right after ESCAPE-CHAR are part of
+a word (ESCAPE-CHAR protects a char from having a special meaning).
 
-If UNESCAPE is non-NIL remove ESCAPE-CHARs (when they are not escaped
-itself) from the output; if UNESCAPE is NIL interpret ESCAPE-CHARs but
-don't remove them.
+If UNESCAPE is non-NIL interpret and remove ESCAPE-CHARs (when they are
+not escaped itself) from the output; if UNESCAPE is NIL interpret
+ESCAPE-CHARs but don't remove them.
 
-If there is opening quote but the closing quote is missing signal
-CLOSING-QUOTE-MISSING condition (subtype of CL:END-OF-FILE). The
+If there is opening quote in the input but the closing quote is missing
+signal CLOSING-QUOTE-MISSING condition (subtype of CL:END-OF-FILE). The
 condition object contains the stream and it can be read with
 CL:STREAM-ERROR-STREAM function. The currently parsed string can be read
-with PARSED-STRING function. There is CURRENT-STRING restart available.
-When it is invoked the function continues and accepts the currently
-parsed string (if any). Another restart NEW-STRING must be invoked with
-a new string value which is then used and returned."
+with PARSED-STRING function. There are two restarts available. When
+CURRENT-STRING restart is invoked the function continues and accepts the
+currently parsed string (if any). Another restart NEW-STRING must be
+invoked with a new string value which is then used and returned."
 
   (check-type output-stream (or stream null))
-  (check-type quote-char (or character null))
+  (check-type quote-start-char (or character null))
+  (check-type quote-end-char (or character null))
+  (assert (or (and quote-start-char quote-end-char)
+              (and (null quote-start-char)
+                   (null quote-end-char)))
+          nil "Must define both or neither of QUOTE-START-CHAR and ~
+                QUOTE-END-CHAR.")
   (check-type escape-char (or character null))
+  (assert (or (null escape-char)
+              (and escape-char
+                   (not (eql escape-char quote-start-char))
+                   (not (eql escape-char quote-end-char))))
+          nil "ESCAPE-CHAR (if defined) must not be same character as ~
+                QUOTE-START-CHAR or QUOTE-END-CHAR.")
   (check-type separator-chars sequence)
 
   (labels ((separator-char-p (char)
              (find char separator-chars))
-           (quote-char-p (char)
-             (char= char quote-char))
-           (escape-char-p (char)
-             (char= char escape-char))
            (parse-stream (in out)
              (let ((quote nil))
                (handler-case
@@ -78,18 +92,20 @@ a new string value which is then used and returned."
                                  (not esc))
                             (return))
                            ((and escape-char
-                                 (escape-char-p char)
+                                 (char= escape-char char)
                                  (not esc))
                             (setf esc t)
                             (unless unescape
                               (vector-push-extend char out)))
-                           ((and quote-char
-                                 (quote-char-p char)
+                           ((and quote-start-char
+                                 quote-end-char
+                                 (char= quote-start-char char)
                                  (not quote)
                                  (not esc))
                             (setf quote t))
-                           ((and quote-char
-                                 (quote-char-p char)
+                           ((and quote-start-char
+                                 quote-end-char
+                                 (char= quote-end-char char)
                                  quote
                                  (not esc))
                             (setf quote nil))
@@ -102,9 +118,7 @@ a new string value which is then used and returned."
                                     :stream (stream-error-stream c)
                                     :string out)))))))
 
-    (let ((output (make-array 20 :element-type 'character
-                                 :adjustable t
-                                 :fill-pointer 0)))
+    (let ((output (make-adjustable-string 20)))
       (restart-case
           (etypecase string-or-stream
             (string (with-input-from-string (s string-or-stream)
@@ -113,7 +127,7 @@ a new string value which is then used and returned."
         (current-string ()
           :report "Continue and accept the current string.")
         (new-string (new)
-          :report "Continue and supply a new string."
+          :report "Supply a new string and continue."
           (check-type new string)
           (setf output new)))
 
@@ -128,17 +142,21 @@ NEEDS-ESCAPING sequence."
   (check-type string string)
   (check-type needs-escaping sequence)
   (check-type escape-char character)
-  (with-output-to-string (out)
+  (let ((out (make-adjustable-string (length string))))
     (loop :for char :across string
           :do (when (find char needs-escaping)
-                (princ escape-char out))
-              (princ char out))))
+                (vector-push-extend escape-char out))
+              (vector-push-extend char out))
+    out))
 
 (defun quote-string (string &key output-stream
-                              (quote-char #\") (escape-char #\\)
-                              (needs-escaping (list quote-char escape-char)))
+                              (quote-start-char #\") (quote-end-char #\")
+                              (escape-char #\\)
+                              (needs-escaping (list quote-start-char
+                                                    quote-end-char
+                                                    escape-char)))
 
-  "Put STRING inside quotes (QUOTE-CHAR).
+  "Put STRING inside quotes (QUOTE-START-CHAR, QUOTE-END-CHAR).
 
 If ESCAPE-CHAR is a non-NIL put it before every character in
 NEEDS-ESCAPING sequence.
@@ -147,18 +165,19 @@ When OUTPUT-STREAM is a stream write the output to that stream."
 
   (check-type string string)
   (check-type output-stream (or stream null))
-  (check-type quote-char character)
+  (check-type quote-start-char character)
+  (check-type quote-end-char character)
   (check-type escape-char (or character null))
   (check-type needs-escaping sequence)
 
   (let ((output
           (concatenate 'string
-                       (string quote-char)
+                       (string quote-start-char)
                        (if escape-char
                            (escape-characters string needs-escaping
                                               escape-char)
                            string)
-                       (string quote-char))))
+                       (string quote-end-char))))
 
     (when output-stream (princ output output-stream))
     output))
@@ -169,11 +188,12 @@ characters.
 
 If OUTPUT-STREAM is a stream write the output to that stream."
   (quote-string string :output-stream output-stream
-                       :quote-char #\'
+                       :quote-start-char #\'
+                       :quote-end-char #\'
                        :escape-char #\'
                        :needs-escaping "'"))
 
-(defun sql-like-escape (string &key output-stream wild-before wild-after)
+(defun sql-escape-like (string &key output-stream wild-before wild-after)
   "Return STRING as an SQL language string ('...') and escape all '
 characters as well as all special characters of SQL LIKE operator. If
 WILD-BEFORE or WILD-AFTER is non-NIL put LIKE operator's % wild card
@@ -185,16 +205,15 @@ If OUTPUT-STREAM is a stream write the output to that stream too."
 
   (check-type output-stream (or stream null))
 
-  (let ((output
-          (concatenate
-           'string
-           (sql-string (concatenate
-                        'string
-                        (if wild-before "%")
-                        (escape-characters string "_%\\" #\\)
-                        (if wild-after "%"))
-                       :output-stream nil)
-           " ESCAPE '\\'")))
+  (let ((output (concatenate
+                 'string
+                 (sql-string (concatenate
+                              'string
+                              (if wild-before "%")
+                              (escape-characters string "_%\\" #\\)
+                              (if wild-after "%"))
+                             :output-stream nil)
+                 " ESCAPE '\\'")))
 
     (when output-stream
       (princ output output-stream))
@@ -214,12 +233,13 @@ character codes."
 
   (labels
       ((read-chars (in predicate max)
-         (with-output-to-string (chars)
+         (let ((chars (make-adjustable-string max)))
            (loop :repeat max
                  :for c := (peek-char nil in)
                  :do (if (funcall predicate c)
-                         (princ (read-char in) chars)
-                         (loop-finish)))))
+                         (vector-push-extend (read-char in) chars)
+                         (loop-finish)))
+           chars))
 
        (hex-char-p (char)
          (digit-char-p char 16))
@@ -237,50 +257,52 @@ character codes."
 
              (let ((sub-char (read-char in)))
                (case sub-char
-                 (#\a (princ #\Bel out))
-                 (#\b (princ #\Backspace out))
-                 (#\e (princ #\Esc out))
-                 (#\f (princ #\Page out))
-                 (#\n (princ #\Newline out))
-                 (#\r (princ #\Return out))
-                 (#\t (princ #\Tab out))
-                 (#\v (princ #\Vt out))
-                 (#\\ (princ #\\ out))
-                 (#\' (princ #\' out))
-                 (#\" (princ #\" out))
-                 (#\? (princ #\? out))
+                 (#\a (vector-push-extend #\Bel out))
+                 (#\b (vector-push-extend #\Backspace out))
+                 (#\e (vector-push-extend #\Esc out))
+                 (#\f (vector-push-extend #\Page out))
+                 (#\n (vector-push-extend #\Newline out))
+                 (#\r (vector-push-extend #\Return out))
+                 (#\t (vector-push-extend #\Tab out))
+                 (#\v (vector-push-extend #\Vt out))
+                 (#\\ (vector-push-extend #\\ out))
+                 (#\' (vector-push-extend #\' out))
+                 (#\" (vector-push-extend #\" out))
+                 (#\? (vector-push-extend #\? out))
 
                  (#\u
                   (let ((cp (read-chars in #'hex-char-p 4)))
                     (when (= 4 (length cp))
-                      (princ (code-char (parse-int-base 16 cp)) out))))
+                      (vector-push-extend (code-char (parse-int-base 16 cp))
+                                          out))))
 
                  (#\U
                   (let ((cp (read-chars in #'hex-char-p 8)))
                     (when (= 8 (length cp))
-                      (princ (code-char (parse-int-base 16 cp)) out))))
+                      (vector-push-extend (code-char (parse-int-base 16 cp))
+                                          out))))
 
                  (#\x
                   (let ((cp (read-chars in #'hex-char-p 2)))
                     (when (plusp (length cp))
-                      (princ (code-char (parse-int-base 16 cp)) out))))
+                      (vector-push-extend (code-char (parse-int-base 16 cp))
+                                          out))))
 
                  (t
                   (unread-char sub-char in)
                   (let ((cp (read-chars in #'octal-char-p 3)))
                     (when (plusp (length cp))
-                      (princ (code-char (parse-int-base 8 cp)) out))))))
+                      (vector-push-extend (code-char (parse-int-base 8 cp))
+                                          out))))))
 
-           :else :do (princ char out))))
+           :else :do (vector-push-extend char out))))
 
-    (let ((output
-            (with-output-to-string (out)
-              (handler-case
-                  (etypecase string-or-stream
-                    (string (with-input-from-string (s string-or-stream)
-                              (parse-stream s out)))
-                    (stream (parse-stream string-or-stream out)))
-                (end-of-file () nil)))))
+    (let ((output (make-adjustable-string 15)))
+      (handler-case (etypecase string-or-stream
+                      (string (with-input-from-string (s string-or-stream)
+                                (parse-stream s output)))
+                      (stream (parse-stream string-or-stream output)))
+        (end-of-file () nil))
 
       (when output-stream
         (princ output output-stream))
